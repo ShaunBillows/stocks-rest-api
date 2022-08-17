2672a4b1da42dd87ddb2edb52a5013131af9bd0f
ShaunBillows <billows.shaun@gmail.com>
2022-08-17 21:27:52 +0100

    removed movie model

--- src/movie/routes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const userRouter = Router();
-const { createUser, readUser, readAllUsers, deleteUser, updateUser, login } = require("./controllers");
-const { hashPass, comparePass, tokenCheck } = require("../middleware");
-
-userRouter.get("/movie", createMovie)
-
-module.exports = userRouter;
